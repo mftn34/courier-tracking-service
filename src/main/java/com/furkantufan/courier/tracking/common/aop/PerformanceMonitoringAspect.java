df3022b3b4d80e060b48029e42f61bb2a473fb2d
furkantufan <furkantufan0127@gmail.com>
2024-12-21 02:09:27 +0300

    performance log aspect added

---
+++ src/main/java/com/furkantufan/courier/tracking/common/aop/PerformanceMonitoringAspect.java
@@ -0,0 +1,22 @@
+package com.furkantufan.courier.tracking.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class PerformanceMonitoringAspect {
+
+    @Around("execution(* com.furkantufan.courier.tracking.service.*.*(..))")
+    public Object monitorPerformance(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        log.info("Execution time of {} {} ms", joinPoint.getSignature().getName(), (endTime - startTime));
+        return result;
+    }
+}
